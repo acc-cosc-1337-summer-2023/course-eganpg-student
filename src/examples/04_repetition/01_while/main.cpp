5c5ded70d2f7ec8214a87265b0ef30bdaac2006c
eganpg-student <135080011+eganpg-student@users.noreply.github.com>
2023-06-08 23:13:35 +0000

    6.6.23 classwork

--- src/examples/04_repetition/01_while/main.cpp
+++ src/examples/04_repetition/01_while/main.cpp
@@ -1,9 +1,18 @@
 #include "while.h"
 using std::string;
+using std::cin; 
+using std::cout;
 
 int main() 
 {
 	string txt = "Hello";
 	display(txt);
+
+	auto num = 0;
+	cout<<"Enter a number: ";
+	cin>>num;
+
+	auto result = sum_of_squares(num);
+	cout<<"Result: "<<result<<"\n";
 	return 0;
 }
