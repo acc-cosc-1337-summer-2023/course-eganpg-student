d55b38723e4b6021e1b36a2a0fc240c992af5545
eganpg-student <135080011+eganpg-student@users.noreply.github.com>
2023-06-09 00:26:28 +0000

    num_ref practice

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -1,4 +1,11 @@
+
+#include<iostream>
+using std::cout;
 int main()
 {
+    auto num = 5;
+    cout<<"Memory number is: "<<&num<<"\n";
+    int &num_ref = num; //save the address of num into num_ref
+    cout<<"Value of num via num_ref "<<num_ref<<"\n";
     return 0;
 }
