c46f536ea052850619ecb4e346370a77f14a172e
eganpg-student <135080011+eganpg-student@users.noreply.github.com>
2023-06-14 00:55:10 +0000

    vector size and capacity 6_13 classwork

--- src/examples/07_vector/01_create/main.cpp
+++ src/examples/07_vector/01_create/main.cpp
@@ -1,4 +1,20 @@
+#include<vector>
+#include<iostream>
+
+using std::cout;
+using std::vector;
+
+
 int main()
 {
+    vector<int> nums{1, 10, 7};
+    cout<<nums[0]<<"\n";
+    nums[0] = 5;
+    cout<<nums[0]<<"\n";
+
+    cout<<"Size: "<<nums.size()<<"\n";
+    cout<<"Capacity: "<<nums.capacity()<<"\n";
+
+
     return 0;
 }
