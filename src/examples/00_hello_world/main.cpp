a9321215780f655149627e99561de9be51c1d247
eganpg-student <135080011+eganpg-student@users.noreply.github.com>
2023-06-24 19:29:21 +0000

    checking quiz answers 6_22_23

--- src/examples/00_hello_world/main.cpp
+++ src/examples/00_hello_world/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
-    std::cout<<"Hello World!";
+    // std::cout<<"Hello World!";
+    // std::string name(3,  'a' );
+    // std::cout<<name;
+     std::string name =  "Johnny" ;
+     std::cout<<name[3];
+
     return 0;
 }
